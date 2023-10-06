97f36e4aa2a1edd89adec838130013580b8abee9
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2023-10-06 16:01:58 -0700

    simplify chat proxy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.0.3
+
+- Changed: Simplified chat proxy, allowing model override
+
 # v0.0.1
 
 - Initial release
--- src/chat/plexchat.ts
+++ src/chat/plexchat.ts
@@ -1,10 +1,10 @@
 import gptTokenzier from "gpt-tokenizer";
-import type { ChatInput, ChatOutput } from "../openai/types";
+import type { ChatInput, ChatOutput, ModelName } from "../openai/types";
 import { LogLevel } from "../scheduler/logger";
 import { ChatManager } from "../scheduler/manager";
 import { getChatWorkers, type ChatEndpointManifest } from "./get-chat-workers";
 
-export type SimpleChatInput = Partial<ChatInput> & Pick<ChatInput, "messages">;
+export type SimpleChatInput = Partial<ChatInput> & Pick<ChatInput, "messages"> & { models?: ModelName[] };
 export interface SimpleChatProxy {
   (input: SimpleChatInput): Promise<ChatOutput>;
 }
@@ -30,30 +30,10 @@ export function plexchat(config: ProxiesConfig) {
     logLevel: config.logLevel ?? LogLevel.Info,
   });
 
-  const gpt4Proxy: SimpleChatProxy = (input: SimpleChatInput) =>
-    manager.submit({
-      tokenDemand: gptTokenzier.encodeChat(input.messages, "gpt-4").length * 1.05 + (input.max_tokens ?? defaultChatInput.max_tokens),
-      models: ["gpt-4", "gpt-4-32k"],
-      input: {
-        ...defaultChatInput,
-        ...input,
-      },
-    });
-
-  const gpt35Proxy: SimpleChatProxy = (input: SimpleChatInput) =>
+  const chatProxy: SimpleChatProxy = (input: SimpleChatInput) =>
     manager.submit({
       tokenDemand: gptTokenzier.encodeChat(input.messages, "gpt-3.5-turbo").length * 1.05 + (input.max_tokens ?? defaultChatInput.max_tokens),
-      models: ["gpt-35-turbo", "gpt-35-turbo-16k"],
-      input: {
-        ...defaultChatInput,
-        ...input,
-      },
-    });
-
-  const gptProxy: SimpleChatProxy = (input: SimpleChatInput) =>
-    manager.submit({
-      tokenDemand: gptTokenzier.encodeChat(input.messages, "gpt-3.5-turbo").length * 1.05 + (input.max_tokens ?? defaultChatInput.max_tokens),
-      models: ["gpt-35-turbo", "gpt-35-turbo-16k", "gpt-4", "gpt-4-32k"],
+      models: input.models ?? ["gpt-35-turbo", "gpt-35-turbo-16k"],
       input: {
         ...defaultChatInput,
         ...input,
@@ -61,8 +41,6 @@ export function plexchat(config: ProxiesConfig) {
     });
 
   return {
-    gptProxy,
-    gpt35Proxy,
-    gpt4Proxy,
+    chatProxy,
   };
 }
