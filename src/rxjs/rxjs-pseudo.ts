45116dee0fdcfa4b34ca4a7cf060fbdb5a2ead72
Chuanqi <chuanqi.sun.dev@gmail.com>
2023-10-21 21:09:58 -0700

    single task stream

--- src/rxjs/rxjs-pseudo.ts
+++ src/rxjs/rxjs-pseudo.ts
@@ -1,122 +1,31 @@
-import { Subject } from "rxjs";
-import { filter, map, scan } from "rxjs/operators";
+import { Subject, filter, map } from "rxjs";
 
 export default {};
 
-export type Action = NewTaskAction | NewWorkerAction | AssignTasksAction;
+export const enum Status {
+  Pending = "pending",
+  Running = "running",
+  Completed = "completed",
+  Failed = "failed",
+}
 
-export type NewTaskAction = {
-  type: "newTask";
+export interface TaskHandle {
   id: string;
-};
-
-export type NewWorkerAction = {
-  type: "newWorker";
-  id: string;
-};
-
-export type AssignTasksAction = {
-  type: "assignTasks";
-  assignments: Assignment[];
-};
-
-export interface ManagerState {
-  pendingTasks: any[];
-  workers: Worker[];
+  status: Status;
+  task: any;
 }
 
-export interface Task {
-  id: string;
-}
+const task$ = new Subject<TaskHandle>();
 
-export interface Worker {
-  id: string;
-  tasks: Task[];
-}
+task$
+  .pipe(
+    filter((handle) => handle.status === Status.Pending),
+    filter(() => true), // Match available worker)
+    map((handle) => ({ ...handle, status: Status.Running })) // update the state of task, then put it back
+    // update the state of task, then put it back
+  )
+  .subscribe(task$);
 
-export interface Assignment {
-  taskId: string;
-  workerId: string;
-}
+task$.subscribe(console.log);
 
-const managerActions$ = new Subject<Action>();
-
-const state$ = managerActions$.pipe(scan(managerStateReducer, { pendingTasks: [], workers: [] }));
-
-const assignments$ = state$.pipe(
-  map((state) => {
-    // naive assignment
-    const assignments: Assignment[] = [];
-    const pendingTasks = [...state.pendingTasks];
-    const workers = [...state.workers];
-    while (pendingTasks.length > 0 && workers.length > 0) {
-      const task = pendingTasks.pop();
-      const worker = workers.pop();
-      if (task && worker) {
-        assignments.push({ taskId: task.id, workerId: worker.id });
-      }
-    }
-    return assignments;
-  }),
-  filter((assignments) => assignments.length > 0)
-);
-
-state$.subscribe(console.log);
-assignments$.subscribe((assignments) => managerActions$.next({ type: "assignTasks", assignments }));
-
-function managerStateReducer(state: ManagerState, action: Action): ManagerState {
-  switch (action.type) {
-    case "newWorker": {
-      const worker = { id: action.id, tasks: [] };
-      return {
-        ...state,
-        workers: [...state.workers, worker],
-      };
-    }
-    case "newTask": {
-      return {
-        ...state,
-        pendingTasks: [...state.pendingTasks, { id: action.id }],
-      };
-    }
-    case "assignTasks": {
-      return {
-        ...state,
-        workers: state.workers.map((worker) => {
-          const assignedTasks: Task[] = action.assignments
-            .filter((assignment) => assignment.workerId === worker.id)
-            .map((assignment) => ({ id: assignment.taskId }));
-          return {
-            ...worker,
-            tasks: [...worker.tasks, ...assignedTasks],
-          };
-        }),
-        pendingTasks: state.pendingTasks.filter((task) => {
-          return !action.assignments.some((assignment) => assignment.taskId === task.id);
-        }),
-      };
-    }
-    default:
-      return state;
-  }
-}
-
-managerActions$.next({ type: "newWorker", id: "1" });
-managerActions$.next({ type: "newTask", id: "1" });
-managerActions$.next({ type: "newTask", id: "2" });
-managerActions$.next({ type: "newTask", id: "3" });
-managerActions$.next({ type: "newWorker", id: "2" });
-managerActions$.next({ type: "newTask", id: "4" });
-managerActions$.next({ type: "newTask", id: "5" });
-
-// SubmitEvent => OnSubmit
-// OnSubmit => ManagerQueueUpdateEvent
-// (ManageQueueUpdateEvent, WorkerQueueUpdateEvent) => OnDispatch
-// OnDispatch =?=> (ManagerQueueUpdateEvent, WorkerQueueUpdateEvent)
-// WorkerQueueUpdateEvent => OnWorkerQueueUpdate
-// OnWorkerQueueUpdate => WorkStartEvent
-// WorkStartEvent =(external fn call with result wrapped)=> WorkDoneEvent<OK, Err>
-// TimerEvent => WorkerQueueUpdateEvent
-// WorkDoneEvent<OK> => (unwrap fn call result)
-// WorkDoneEvent<Err> => HandleError
-// HandleError => (retry ? WorkerQueueUpdateEvent : unwrap fn call result)
+task$.next({ id: "123", status: Status.Pending, task: { hello: "world" } });
