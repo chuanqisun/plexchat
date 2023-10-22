ef9cda4721667d3755f8b6c136caafafb5f32d1d
Chuanqi <chuanqi.sun.dev@gmail.com>
2023-10-22 13:23:59 -0700

    reset prototype

--- src/rxjs/rxjs-pseudo.ts
+++ src/rxjs/rxjs-pseudo.ts
@@ -1,56 +1,20 @@
-import { randomUUID } from "crypto";
-import { Subject, filter, first, map, mergeMap } from "rxjs";
+import { Subject } from "rxjs";
 
 export default {};
 
-export interface TaskHandle {
+interface TaskHandle {
   id: string;
-  task: any;
-  result?: any;
+  data: any;
 }
 
-const task$ = new Subject<TaskHandle>();
-
-task$.subscribe(console.log);
-
-// success stream
-task$
-  .pipe(
-    filter((handle) => !handle.result),
-    mergeMap((handle) => new Promise<TaskHandle>((resolve) => resolve({ ...handle, result: Math.random() })))
-  )
-  .subscribe(task$);
-
-// retry stream
-task$
-  .pipe(
-    filter((handle) => handle.result && handle.result < 0.5),
-    map((handle) => ({ ...handle, result: undefined })) // retry
-  )
-  .subscribe(task$);
-
-async function submit(task: any) {
-  const newHandle = { id: randomUUID(), task };
-  const resultAsync = new Promise((resolve, reject) =>
-    task$
-      .pipe(
-        filter((completeHandle) => completeHandle.id === newHandle.id && completeHandle.result >= 0.5),
-        first()
-      )
-      .subscribe({
-        next: resolve,
-        error: reject,
-      })
-  );
-
-  task$.next(newHandle);
-
-  return resultAsync;
+interface WorkerHandle {
+  id: string;
+  tasks: TaskHandle[];
 }
 
-async function main() {
-  const final = await Promise.all([submit("task 1"), submit("task 2"), submit("task 3")]);
-  console.log("final", JSON.stringify(final, null, 2));
+interface SchedulerState {
+  taskPool: TaskHandle[];
+  workerPool: WorkerHandle[];
 }
 
-main();
+const state$ = new Subject<SchedulerState>();
