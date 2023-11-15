be0c60bd87c245a5344eb76f831285a922f80a30
Chuanqi <chuanqi.sun.dev@gmail.com>
2023-11-15 11:47:24 -0800

    expose LogLevel type

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./chat/plexchat";
 export * from "./controller/timeout";
 export * from "./openai/proxy";
 export * from "./openai/types";
+export * from "./scheduler/logger";
 export * from "./scheduler/manager";
 export * from "./scheduler/types";
 export * from "./scheduler/worker";
